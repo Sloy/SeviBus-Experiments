7817e7c1732b885ef9c2c667f303c11c9e4dc8d0
Sloy <rafa.vazsan@gmail.com>
2014-12-13 15:24:54 +0100

    Creación del proyecto.
    Aquí no hay nada. Proyecto creado desde 0 con Android Studio 1.0.1
    Aquí comienza el desarrollo de SeviBus 4.0
    Bienvenidos

---
+++ app/src/main/java/com/sloydev/sevibus/MainActivity.java
@@ -0,0 +1,28 @@
+package com.sloydev.sevibus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
